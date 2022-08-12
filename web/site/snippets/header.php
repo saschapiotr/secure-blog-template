fcf0cadd3fe73ff1122647e12d3391d4dfd8d5a4
saschapiot <piotr@immerda.ch>
2022-08-12 16:54:44 +0200

    refactor: css in style folder

--- web/site/snippets/header.php
+++ web/site/snippets/header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <title><?= $site->title() ?> | <?= $page->title() ?></title>
   <?= css([
-    'style.css'
+    'style/index.css'
   ]) ?>
   <!--<link rel="shortcut icon" type="image/x-icon" href="<?= url('favicon.ico') ?>">-->
 </head>
