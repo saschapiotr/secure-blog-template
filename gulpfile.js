07230a718e1679408d58ab5efb84d9db7a9548df
saschapiot <piotr@immerda.ch>
2022-08-13 15:12:19 +0200

    feat: sourcemapping built js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 const { series, src, dest } = require("gulp");
+const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 
 function js(cb) {
   src(['web/site/snippets/**/*js', 'web/site/templates/**/*js'])
+    .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(concat('index.min.js'))
     .pipe(dest('web/assets/js'));
   cb();
